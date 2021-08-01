c9d4f4da22d55d4ed4f5ff32e48374b88ad6240d
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-01 17:06:27 +0800

    feat: Add template

---
+++ src/TShockPluginTemplate/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+
+namespace $safeprojectname$
+{
+    [ApiVersion(2, 1)]
+    public class Plugin : TerrariaPlugin
+    {
+        public override string Name => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyTitleAttribute>().Title;
+
+        public override string Description => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
+
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        public override string Author => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyCompanyAttribute>().Company;
+
+        public Plugin(Main game) : base(game)
+        {
+            Console.WriteLine($"    {Name}  Copyright (C) $year$  {Author}");
+            Console.WriteLine($"    This program comes with ABSOLUTELY NO WARRANTY; see {Name}.COPYING for details");
+            Console.WriteLine($"    This is free software, and you are welcome to redistribute it");
+            Console.WriteLine($"    under certain conditions; see {Name}.COPYING for details.");
+        }
+
+        public override void Initialize()
+        {
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
