4e39b1083ac4db748a212782b753ad2e426537e7
Arthri <41360489+Arthri@users.noreply.github.com>
2021-10-21 00:01:35 +0800

    feat: Change naming style

--- src/TShockPluginTemplate/Plugin.cs
+++ src/TShockPluginTemplate/Plugin.cs
@@ -3,24 +3,24 @@ using System.Reflection;
 using Terraria;
 using TerrariaApi.Server;
 
-namespace $safeprojectname$
+namespace $pluginprefixedname$
 {
     [ApiVersion(2, 1)]
-    public class Plugin : TerrariaPlugin
+    public class $pluginname$ : TerrariaPlugin
     {
         /// <inheritdoc />
-        public override string Name => typeof(Plugin).Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
+        public override string Name => typeof($pluginname$).Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
 
         /// <inheritdoc />
-        public override string Description => typeof(Plugin).Assembly.GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
+        public override string Description => typeof($pluginname$).Assembly.GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
 
         /// <inheritdoc />
-        public override Version Version => typeof(Plugin).Assembly.GetName().Version;
+        public override Version Version => typeof($pluginname$).Assembly.GetName().Version;
 
         /// <inheritdoc />
-        public override string Author => typeof(Plugin).Assembly.GetCustomAttribute<AssemblyCompanyAttribute>().Company;
+        public override string Author => typeof($pluginname$).Assembly.GetCustomAttribute<AssemblyCompanyAttribute>().Company;
 
-        public Plugin(Main game) : base(game)
+        public $pluginname$(Main game) : base(game)
         {
         }
 
