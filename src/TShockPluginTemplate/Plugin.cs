299fba319c93ba9e6599e50b8abe8c1667867f7c
Arthri <41360489+Arthri@users.noreply.github.com>
2021-10-20 23:37:11 +0800

    feat: Inheritdoc for plugin overrides

--- src/TShockPluginTemplate/Plugin.cs
+++ src/TShockPluginTemplate/Plugin.cs
@@ -8,22 +8,28 @@ namespace $safeprojectname$
     [ApiVersion(2, 1)]
     public class Plugin : TerrariaPlugin
     {
+        /// <inheritdoc />
         public override string Name => typeof(Plugin).Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
 
+        /// <inheritdoc />
         public override string Description => typeof(Plugin).Assembly.GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
 
+        /// <inheritdoc />
         public override Version Version => typeof(Plugin).Assembly.GetName().Version;
 
+        /// <inheritdoc />
         public override string Author => typeof(Plugin).Assembly.GetCustomAttribute<AssemblyCompanyAttribute>().Company;
 
         public Plugin(Main game) : base(game)
         {
         }
 
+        /// <inheritdoc />
         public override void Initialize()
         {
         }
 
+        /// <inheritdoc />
         protected override void Dispose(bool disposing)
         {
             if (disposing)
